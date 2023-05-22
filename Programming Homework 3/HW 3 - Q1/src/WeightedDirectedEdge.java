73b5824a18548afaa90916d71b6aef4bde24ed39
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-05-22 11:35:42 +0300

    Update WeightedDirectedEdge.java

--- Programming Homework 3/HW 3 - Q1/src/WeightedDirectedEdge.java
+++ Programming Homework 3/HW 3 - Q1/src/WeightedDirectedEdge.java
@@ -1,7 +1,7 @@
 //-----------------------------------------------------
 // Title: WeightedDirectedEdge class
-// Author: Mustafa Baran Ercan
-// ID: 28810555206
+// Author: Mustafa Baran Ercan, Bedir Esen
+// ID: 28810555206, 15203509106
 // Section: 1
 // Assignment: 3
 // Description: This class implements the Weighted Directed Edge Object.
