3fc8b9075e10989599e1e05c7c41055781d08330
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-05-22 11:33:45 +0300

    Update Bag.java

--- Programming Homework 3/HW 3 - Q1/src/Bag.java
+++ Programming Homework 3/HW 3 - Q1/src/Bag.java
@@ -1,7 +1,7 @@
 //-----------------------------------------------------
 // Title: Bag class
-// Author: Mustafa Baran Ercan
-// ID: 28810555206
+// Author: Mustafa Baran Ercan, Bedir Esen
+// ID: 28810555206, 15203509106
 // Section: 1
 // Assignment: 3
 // Description: This class is the Java Bag implementation.
