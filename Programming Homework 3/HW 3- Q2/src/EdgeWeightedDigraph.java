5d29ab69dc4902ac4566f0bb8ab8ff97d1d145b4
MBaranErcan <102927124+MBaranErcan@users.noreply.github.com>
2023-05-27 15:22:53 +0300

    Add files via upload

---
+++ Programming Homework 3/HW 3- Q2/src/EdgeWeightedDigraph.java
@@ -0,0 +1,34 @@
+//-----------------------------------------------------
+// Title: EdgeWeightedDigraph class
+// Author: Mustafa Baran Ercan, Bedir Esen
+// ID: 28810555206, 15203509106
+// Section: 1
+// Assignment: 3
+// Description: This class implements an EdgeWeightedDigraph methods.
+//-----------------------------------------------------
+
+public class EdgeWeightedDigraph {
+    private final int V;                                // Number of vertices.
+    private final Bag<WeightedDirectedEdge>[] adj;      // Array of bags to represent adjacency lists.
+
+    public EdgeWeightedDigraph(int V) {                 // Constructor.
+        this.V = V;                                     // Set V.
+        adj = (Bag<WeightedDirectedEdge>[]) new Bag[V]; // Initialize adj list.
+        for (int v = 0; v < V; v++) {
+            adj[v] = new Bag<WeightedDirectedEdge>();   // Initialize each adjacency list.
+        }
+    }
+
+    public void addEdge(WeightedDirectedEdge e) {       // Method to add an edge to the graph.
+        int v = e.from();                               // Get the "from" vertex.
+        adj[v].add(e);                                  // Add the edge to the "from" vertex's adjacency list.
+    }
+
+    public Iterable<WeightedDirectedEdge> adj(int v) {  // Returns the adjacency list of a vertex.
+        return adj[v];
+    }
+
+    public int V() {                                    // Returns the number of vertices in the graph.
+        return V;
+    }
+}
---
+++ Programming Homework 3/HW 3- Q2/src/WeightedDirectedEdge.java
@@ -0,0 +1,35 @@
+//-----------------------------------------------------
+// Title: WeightedDirectedEdge class
+// Author: Mustafa Baran Ercan, Bedir Esen
+// ID: 28810555206, 15203509106
+// Section: 1
+// Assignment: 3
+// Description: This class implements the Weighted Directed Edge Object.
+//-----------------------------------------------------
+
+public class WeightedDirectedEdge implements Comparable<WeightedDirectedEdge> {
+    private final int v;                                    // The source vertex of the edge.
+    private final int w;                                    // The destination vertex of the edge.
+    private final int weight;                               // The weight of the edge.
+
+    public WeightedDirectedEdge(int v, int w, int weight) { // Constructor that takes the
+        this.v = v;                                         // Source vertex,
+        this.w = w;                                         // Destination vertex,
+        this.weight = weight;                               // And weight as input.
+    }
+
+    public int from() {                                     // Returns the source vertex of the edge.
+        return v;
+    }
+
+    public int to() {                                       // Returns the destination vertex of the edge.
+        return w;
+    }
+
+    public int weight() {                                   // Returns the weight of the edge.
+        return weight;
+    }
+
+    public int compareTo(WeightedDirectedEdge other) {      // Compares two edges by their weights.
+        return Integer.compare(weight, other.weight);
+    }}
